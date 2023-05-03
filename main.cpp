17251c0d7407e3d9cb08586efc2740ce8ac40f78
adamcesco <acescobedo@smu.edu>
2023-05-03 02:37:57 -0500

    hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+search engine
+kaggle dataset handler - indexer
+query engine
+indexer
+ui
+*/
+
+int main(int argc, char** argv){
+    std::cout << "hello world" << std::endl;
+}
