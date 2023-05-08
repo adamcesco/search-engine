067c47f98344915fb27bddc3c894d02b906c2582
adamcesco <acescobedo@smu.edu>
2023-05-08 17:17:46 -0500

    templated RunTimeDatabase

--- ParseEngine.h
+++ ParseEngine.h
@@ -16,33 +16,40 @@ namespace search_engine {
 
 namespace parse_util {
 
-struct RunTimeDataBase {
-    std::unordered_map<std::string, std::string> id_map;                                                 // uuid -> file path
-    std::vector<std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>>> text_index;  // word -> list of {uuid -> count}
-    std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> title_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> site_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> language_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> location_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> person_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> organization_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> author_index;
-    std::unordered_map<std::string, std::unordered_set<std::string>> country_index;
+/*! @brief Define as a template struct to allow for the use of different data types for the values to be indexed (T) and IDs of the sources (U) you plan to store in the database.
+ * @tparam T The data type of the values to be indexed.
+ * @tparam U The data type of the IDs of the sources you plan to store in the database.
+ * @warning The use of this struct is not recommended outside of a child class of the ParseEngine class.
+ * @format The format of the data stored in the database is as follows:
+ */
+template <typename T, typename U>
+struct RunTimeDatabase {
+    std::unordered_map<U, std::string> id_map;                                       // uuid -> file path
+    std::vector<std::unordered_map<T, std::unordered_map<U, uint32_t>>> text_index;  // word -> list of {uuid -> count}
+    std::unordered_map<T, std::unordered_map<U, uint32_t>> title_index;
+    std::unordered_map<T, std::unordered_set<U>> site_index;
+    std::unordered_map<T, std::unordered_set<U>> language_index;
+    std::unordered_map<T, std::unordered_set<U>> location_index;
+    std::unordered_map<T, std::unordered_set<U>> person_index;
+    std::unordered_map<T, std::unordered_set<U>> organization_index;
+    std::unordered_map<T, std::unordered_set<U>> author_index;
+    std::unordered_map<T, std::unordered_set<U>> country_index;
 };
 
 class ParseEngine {
    public:
     /*!
      * @warning The optional `stop_words_ptr` pointer parameter, if supplied to this function, must outlive this functions entire execution.
-     * @param file_path The file path of the file or folder of files you desire to parse and fill a RunTimeDataBase object with.
+     * @param file_path The file path of the file or folder of files you desire to parse and fill a RunTimeDatabase object with.
      * @param stop_words_ptr An optional parameter that is a constant pointer to an unordered_set of stop words.
      */
     virtual void Parse(std::string file_path, const std::unordered_set<std::string>* stop_words_ptr = NULL) = 0;
 
     /*!
-     * @brief Returns the RunTimeDataBase owned by the invoked ParseEngine object.
+     * @brief Returns the RunTimeDatabase owned by the invoked ParseEngine object.
      * @warning The return value should not be deleted, and the use of the return value should be restricted to the lifetime of the invoked ParseEngine object.
      */
-    virtual inline const RunTimeDataBase* GetRunTimeDatabase() const = 0;
+    virtual inline const RunTimeDatabase<std::string, std::string>* GetRunTimeDatabase() const = 0;
 };
 
 }  // namespace parse_util
@@ -52,7 +59,7 @@ class KaggleFinanceParseEngine : public parse_util::ParseEngine {
    public:
     explicit KaggleFinanceParseEngine(size_t parse_amount, size_t fill_amount);
     void Parse(std::string file_path, const std::unordered_set<std::string>* stop_words = NULL) override;
-    inline const parse_util::RunTimeDataBase* GetRunTimeDatabase() const override { return &database_; };
+    inline const parse_util::RunTimeDatabase<std::string, std::string>* GetRunTimeDatabase() const override { return &database_; };
 
    private:
     struct ParsingThreadArgs {
@@ -76,7 +83,7 @@ class KaggleFinanceParseEngine : public parse_util::ParseEngine {
     static void* ArbitratorThreadFunc(void* _arg);  // consumer and producer
     static void* FillingThreadFunc(void* _arg);     // consumer
 
-    parse_util::RunTimeDataBase database_;
+    parse_util::RunTimeDatabase<std::string, std::string> database_;  // todo: change database_ to be a map of size_t (hashed words) to size_t (hashed uuids)
     std::vector<std::pair<std::string, std::unordered_map<std::string, uint32_t>>> unformatted_database_;
     std::vector<std::filesystem::__cxx11::path> files_;
     size_t parsing_thread_count_;
--- SearchEngine.h
+++ SearchEngine.h
@@ -9,16 +9,28 @@
 
 namespace search_engine {
 
+template <typename T, typename U>
 class SearchEngine {
    public:
-    SearchEngine(std::unique_ptr<parse_util::RunTimeDataBase>&& dep_inj_ptr) : database_ptr_{std::forward<std::unique_ptr<parse_util::RunTimeDataBase>>(dep_inj_ptr)} {}
+    SearchEngine(std::unique_ptr<parse_util::RunTimeDatabase<T, U>>&& dep_inj_ptr) : database_ptr_{std::forward<std::unique_ptr<parse_util::RunTimeDatabase<T, U>>>(dep_inj_ptr)} {}
     void DisplayConsoleUserInterface(std::optional<std::string> shortcut = std::nullopt);
 
    private:
     std::unordered_set<std::string> HandleQuery(std::string query);
 
-    std::unique_ptr<parse_util::RunTimeDataBase> database_ptr_;
+    std::unique_ptr<parse_util::RunTimeDatabase<T, U>> database_ptr_;
 };
+
+template <typename T, typename U>
+void SearchEngine<T, U>::DisplayConsoleUserInterface(std::optional<std::string> shortcut) {
+    // todo: implement function
+}
+
+template <typename T, typename U>
+std::unordered_set<std::string> SearchEngine<T, U>::HandleQuery(std::string query) {
+    // todo: implement function
+    return std::unordered_set<std::string>();
+}
 
 }  // namespace search_engine
 
